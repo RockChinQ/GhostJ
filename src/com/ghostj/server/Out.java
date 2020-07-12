8556b5254af76f8b0ff29c0e64c64770125146ae
Rock Chin <1010553892@qq.com>
2020-07-12 10:00:53 +0800

    add a description

--- src/com/ghostj/server/Out.java
+++ src/com/ghostj/server/Out.java
@@ -7,6 +7,7 @@ public class Out {
     public static void say(String sub,String msg){
         say("["+sub+"]"+msg);
     }
+    //在此行输出
     public static void sayThisLine(char msg){
         System.out.print(msg);
     }
