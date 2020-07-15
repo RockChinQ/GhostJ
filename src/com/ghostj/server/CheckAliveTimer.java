d12bf306689c8070fad266b2b55fb39c5c182132
Rock Chin <1010553892@qq.com>
2020-07-15 21:06:24 +0800

    终极优化在线检测，timer绝对不会结束

--- src/com/ghostj/server/CheckAliveTimer.java
+++ src/com/ghostj/server/CheckAliveTimer.java
@@ -9,30 +9,35 @@ import java.util.TimerTask;
 public class CheckAliveTimer extends TimerTask {
     public void run(){
         if(!ServerMain.manuallyTestConn) {
-            ArrayList<HandleConn> dead = new ArrayList<>();
-            for (HandleConn handleConn : ServerMain.socketArrayList) {
-                try {
-                    handleConn.success = false;
-                    CheckConnAlive cca = new CheckConnAlive(handleConn.bufferedWriter);
-                    cca.start();
-                    new Thread().sleep(1500);
-                    if (!handleConn.success)
+            try {
+                ArrayList<HandleConn> dead = new ArrayList<>();
+                for (HandleConn handleConn : ServerMain.socketArrayList) {
+                    try {
+                        handleConn.success = false;
+                        CheckConnAlive cca = new CheckConnAlive(handleConn.bufferedWriter);
+                        cca.start();
+                        new Thread().sleep(1500);
+                        if (!handleConn.success)
+                            dead.add(handleConn);
+                    } catch (Exception e) {
                         dead.add(handleConn);
-                } catch (Exception e) {
-                    dead.add(handleConn);
+                    }
                 }
-            }
-            //Out.say("CheckAliveTimer","检查到"+dead.size()+"个失效连接");
-            for (HandleConn d : dead) {
-                try {
-                    ServerMain.killConn(d);
-                }catch (Exception e){
-                    Out.say("CheckAliveTimer","清除失效连接时出现问题");
-                    e.printStackTrace();
+                //Out.say("CheckAliveTimer","检查到"+dead.size()+"个失效连接");
+                for (HandleConn d : dead) {
+                    try {
+                        ServerMain.killConn(d);
+                    } catch (Exception e) {
+                        Out.say("CheckAliveTimer", "清除失效连接时出现问题");
+                        e.printStackTrace();
+                    }
                 }
+                dead = null;
+                System.gc();
+            }catch (Exception e){
+                Out.say("CheckAliveTimer","检测连接时出错");
+                e.printStackTrace();
             }
-            dead = null;
-            System.gc();
         }
     }
 }
