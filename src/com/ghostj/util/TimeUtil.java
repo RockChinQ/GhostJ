0491cdee4a726e2e7e4c1ce092d303014b962b73
RockChin <1010553892@qq.com>
2020-08-13 17:54:39 +0800

    修复TimeUtil月份显示的bug

--- src/com/ghostj/util/TimeUtil.java
+++ src/com/ghostj/util/TimeUtil.java
@@ -5,6 +5,6 @@ import java.util.Date;
 public class TimeUtil {
     public static String millsToMMDDHHmmSS(long mills){
         Date d=new Date(mills);
-        return d.getMonth()+"."+d.getDate()+","+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
+        return (d.getMonth()+1)+"."+d.getDate()+","+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
     }
 }
