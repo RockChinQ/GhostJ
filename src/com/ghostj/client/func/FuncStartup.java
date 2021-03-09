dc96f4c52031add1d2301dc46553988d7afae330
Rock Chin <1010553892@qq.com>
2021-03-09 11:38:23 +0800

    perfect client startup

--- src/com/ghostj/client/func/FuncStartup.java
+++ src/com/ghostj/client/func/FuncStartup.java
@@ -2,7 +2,9 @@ package com.ghostj.client.func;
 
 import com.ghostj.client.cmd.AbstractFunc;
 import com.ghostj.client.cmd.AbstractProcessor;
+import com.ghostj.client.conn.HandleConn;
 import com.ghostj.client.core.ClientMain;
+import com.ghostj.client.util.Out;
 
 public class FuncStartup implements AbstractFunc {
     @Override
@@ -29,7 +31,7 @@ public class FuncStartup implements AbstractFunc {
     public void run(String[] params, String cmd, AbstractProcessor processor) {
 
         //运行启动任务
-        if (ClientMain.config.field.containsKey("startupRoutine")){
+        if (ClientMain.config.field.containsKey("startupRoutine")&&params.length==0){
             try {
                 processor.start("!!proc new startup");
                 Thread.sleep(1000);
@@ -40,6 +42,25 @@ public class FuncStartup implements AbstractFunc {
             } catch (Exception e) {
                 System.out.println("failed to run startupRoutine");
                 e.printStackTrace();
+            }
+        }else if (params.length>=1){
+            switch (params[0]){
+                case "set":{
+                    if (params.length>1){
+                        ClientMain.config.set("startupRoutine",cmd.substring(14));
+                        ClientMain.config.write();
+                        HandleConn.writeToServerIgnoreException("设置启动项:"+ClientMain.config.getStringValue("startupRoutine"));
+                    }else {
+                        HandleConn.writeToServerIgnoreException("命令语法不正确");
+                    }
+                    break;
+                }
+                case "rm":{
+                    ClientMain.config.remove("startupRoutine");
+                    ClientMain.config.write();
+                    HandleConn.writeToServerIgnoreException("删除启动项");
+                    break;
+                }
             }
         }
     }
